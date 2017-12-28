8c0fb06cead8fbcd3deb03027db4c1ca511f3cdd
Ilya Pikulin <pseudo.ilya@gmail.com>
2017-12-28 03:53:01 +0300

    Remove unnecessary tokens

--- src/fib.hs
+++ src/fib.hs
@@ -1,3 +1,3 @@
-main = interact $ show . run . map (read :: String -> Int) . words where
+main = interact $ show . run . map read . words where
     run (n:k:[]) = fib !! (n-1) where
-        fib = [1, 1] ++ (zipWith (+) (tail fib) (map (k*) fib))
+        fib = [1, 1] ++ zipWith (+) (tail fib) (map (k*) fib)
