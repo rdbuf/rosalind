ea73f4e37ddf93244a6e23cb727d18fd5b37b43b
Ilya Pikulin <pseudo.ilya@gmail.com>
2018-01-20 01:50:22 +0300

    Rework the solution (count the ans in one pass, be more concise)

--- src/gc.hs
+++ src/gc.hs
@@ -1,8 +1,25 @@
-import Data.List.Split
-import Data.Ratio
-import Data.Char
+import Data.List.Split (wordsBy)
+import Data.List (maximumBy, foldl')
+import Data.Char (isAlpha, isSpace)
+import Data.Bifunctor (second)
+import Data.Tuple (snd)
+import Data.Function (on)
+import Data.Monoid ((<>))
+import Data.Foldable (foldMap)
+import Foreign.Marshal.Utils (fromBool)
 
-main = interact $ format . maximum . map (run . lines) . wordsBy (=='>') where
-    run (label:dnaParts) = ((fromIntegral . length . filter (`elem` "GC")) dna / (fromIntegral . length) dna, label) where
-        dna = (filter isAlpha . unlines) dnaParts
-    format (ratio, title) = title ++ "\n" ++ show (100 * ratio)
+-- TODO:
+-- - Profile memory usage
+-- - Employ parsec
+
+data Percentage = Percentage { numerator :: !Integer, denominator :: !Integer }
+
+instance Monoid Percentage where
+    mempty = Percentage 0 0
+    mappend (Percentage nL dL) (Percentage nR dR) = Percentage (nL + nR) (dL + dR)
+
+main = interact $ format . maximumBy (compare `on` snd) . map (second (getPercentage . foldMap percentage . filter isAlpha) . break isSpace) . wordsBy (=='>') where
+    percentage = flip Percentage 1 . fromBool . flip elem "GC"
+    getPercentage (Percentage numerator 0) = 0
+    getPercentage (Percentage numerator denominator) = 100.0 * (fromIntegral numerator) / (fromIntegral denominator)
+    format = uncurry ((<>) . flip (<>) "\n") . second show
