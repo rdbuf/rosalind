923cfdca83061df869076a7cacf9034eb12c4ef1
Ilya Pikulin <pseudo.ilya@gmail.com>
2018-01-20 20:54:31 +0300

    Rewrite being inspired by [1]
    [1]: https://twdkz.wordpress.com/2013/05/31/data-analysis-with-monoids/

--- src/gc.hs
+++ src/gc.hs
@@ -1,16 +1,23 @@
+{-# LANGUAGE TypeFamilies #-}
+
+import Control.Applicative (liftA2)
 import Data.List.Split (wordsBy)
-import Data.List (maximumBy, foldl')
 import Data.Char (isAlpha, isSpace)
 import Data.Bifunctor (second)
-import Data.Tuple (snd)
-import Data.Function (on)
 import Data.Monoid ((<>))
 import Data.Foldable (foldMap)
 import Foreign.Marshal.Utils (fromBool)
 
+
 -- TODO:
 -- - Profile memory usage
 -- - Employ parsec
+
+
+class Aggregation a where
+    type Summarize a :: *
+    summarize :: a -> Summarize a
+
 
 data Percentage = Percentage { numerator :: !Integer, denominator :: !Integer }
 
@@ -18,9 +25,29 @@ instance Monoid Percentage where
     mempty = Percentage 0 0
     mappend (Percentage nL dL) (Percentage nR dR) = Percentage (nL + nR) (dL + dR)
 
-main = interact $ format . maximumBy (compare `on` snd) . map processItem . wordsBy (=='>') where
-    processItem = second (getPercentage . foldMap percentage . filter isAlpha) . break isSpace where
-        percentage = flip Percentage 1 . fromBool . flip elem "GC"
-        getPercentage (Percentage numerator 0) = 0
-        getPercentage (Percentage numerator denominator) = 100.0 * (fromIntegral numerator) / (fromIntegral denominator)
-    format = uncurry ((<>) . flip (<>) "\n") . second show
+instance Aggregation Percentage where
+    type Summarize (Percentage) = Double
+    summarize (Percentage numerator 0) = 0.0
+    summarize (Percentage numerator denominator) = 100.0 * (fromIntegral numerator) / (fromIntegral denominator)
+
+
+data MaxItem = MaxItem { name :: !String, value :: !Double } | EmptyMaxItem
+
+instance Monoid MaxItem where
+    mempty = EmptyMaxItem
+    mappend (MaxItem nameL valueL) (MaxItem nameR valueR) 
+        | valueL < valueR = MaxItem nameR valueR
+        | otherwise = MaxItem nameL valueL
+    mappend (MaxItem name value) EmptyMaxItem = MaxItem name value
+    mappend EmptyMaxItem (MaxItem name value) = MaxItem name value
+    mappend EmptyMaxItem EmptyMaxItem = EmptyMaxItem
+
+instance Aggregation MaxItem where
+    type Summarize MaxItem = String
+    summarize EmptyMaxItem = "EmptyMaxItem"
+    summarize (MaxItem name value) = name <> "\n" <> show value
+
+
+main = interact $ summarize . foldMap processItem . wordsBy (=='>') where
+    processItem = uncurry MaxItem . second (summarize . foldMap percentage) . break isSpace where
+        percentage = liftA2 Percentage (fromBool . flip elem "GC") (fromBool . isAlpha)
